3fee519559a4395fd725ff9c6462b7410c317cce
Rafa V. Cunha <134729430+RafaVitorCunha@users.noreply.github.com>
2024-10-24 14:26:51 -0300

    Create Residencial.php

---
+++ OrientacaoObjetos/AtividadesAvaliativas/ConsumoEnergia/modelo/Residencial.php
@@ -0,0 +1,18 @@
+<?php
+    require_once("IConsumidor.php");
+    class Residencial implements IConsumidorEnergia {
+        private $consumo;
+
+        public function getValorFatura() {
+            return $this->consumo * 1.05;
+        }
+
+        public function getConsumo() {
+            return $this->consumo;
+        }
+
+        public function setConsumo($consumo): self {
+            $this->consumo = $consumo;
+            return $this;
+        }
+    }
