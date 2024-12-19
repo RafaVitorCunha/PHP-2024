dca3a7082e50bfd88925089bb70f879b448f649d
Rafa V. Cunha <134729430+RafaVitorCunha@users.noreply.github.com>
2024-12-19 15:30:42 -0300

    Create Desenvolvedor.php

---
+++ OrientacaoObjetos/AtividadeEmpresa/modelo/Desenvolvedor.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("Funcionario.php");
+
+    class Desenvolvedor extends Funcionario {
+        public function getSalario() {
+            return 1500 * 2.3;
+        }
+
+        public function getCargo() {
+            return "Desenvolvedor";
+        }
+    }
+?>
