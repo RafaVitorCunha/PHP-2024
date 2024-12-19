4ef7a8b26ce1021231a0cb7dd34bac178872a66d
Rafa V. Cunha <134729430+RafaVitorCunha@users.noreply.github.com>
2024-12-19 15:31:00 -0300

    Create Estagiario.php

---
+++ OrientacaoObjetos/AtividadeEmpresa/modelo/Estagiario.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("Funcionario.php");
+
+    class Estagiario extends Funcionario {
+        public function getSalario() {
+            return 1500 * 0.8;
+        }
+
+        public function getCargo() {
+            return "Estagiário";
+        }
+    }
+?>
